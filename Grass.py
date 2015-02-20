0f74cf8a4ad113c80a3c4eec9b8c760cd8f2e0b3
hikaruAi <jats00999@yahoo.es>
2015-02-19 21:40:45 -0600

    Changed random HSV color to random choice between a tuple of colors

--- Grass.py
+++ Grass.py
@@ -1,24 +1,15 @@
 from __future__ import division
 from panda3d.core import LODNode, NodePath,VBase4
-from random import random
-
-import colorsys
+from random import random, choice
 
 class GrassBlade(NodePath):
-    def __init__(self,randomScale=True,randomColor=True,scaleFactor=0.4,baseColor=VBase4(230/255,245/255,175/255,1)):
+    def __init__(self,randomScale=True,randomColor=True,scaleFactor=0.1,baseColor=VBase4(230/255,245/255,175/255,1)):
         NodePath.__init__(self,LODNode("Grass"))
         self.lod0=base.loader.loadModel("3d/grass")
         self.lod0.reparentTo(self)
-        self.node().addSwitch(9999,0)
+        self.node().addSwitch(10,0)
         if randomScale:
-            self.setScale(scaleFactor+(random()/5))
+            self.setScale(.5,.5,scaleFactor+(random()/2))
         else: self.setScale(scaleFactor)
-        color=baseColor
-        if randomColor:
-            hsvT=colorsys.rgb_to_hsv(color.getX(),color.getY(),color.getZ())
-            hsv=list(hsvT)
-            hsv[1]=hsv[1]+random()/4
-            #hsv[0]=hsv[0]-random()/5
-            hsv=colorsys.hsv_to_rgb(hsv[0],hsv[1],hsv[2])
-            color=VBase4(hsv[0],hsv[1],hsv[2],1)
-        self.lod0.setColorScale(color)
+        colors=(baseColor,VBase4(252/255,244/255,88/255,1),VBase4(145/255,179/255,23/255,1),VBase4(44/255,179/255,23/255,1))
+        self.lod0.setColorScale(choice(colors))
